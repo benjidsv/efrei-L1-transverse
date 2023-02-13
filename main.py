c27f411fda68297767079fb8501f60e4ca3c4e1f
benjioff <benjamin.arbousset@gmail.com>
2023-02-13 12:59:07 +0100

    Initialize VCS

---
+++ main.py
@@ -0,0 +1,126 @@
+import pygame as pg
+from constants import *
+from drawables import *
+
+
+class Game:
+    def __init__(self):
+        self.win = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("Projet Transverse Prototype")
+
+        self.running = False
+        self.main_menu_active = False
+        #self.pause_menu_active = False
+        self.clock = pg.time.Clock()
+        self.background = GameBackground()
+        self.ball = Ball(self)
+        self.gk = GoalKeeper(self)
+        self.goal = Goal()
+        self.score = 0
+
+    def draw_window(self, *drawables):
+        for d in drawables:
+            d.draw(self.win)
+
+        #pg.draw.circle(self.win, RED, (WIDTH / 2, GOAL_Y), 3)
+
+        pg.display.update()
+
+    def check_goal(self):
+        if self.gk.try_catching(self.ball):
+            print("catch")
+        else:
+            if self.goal.rect.colliderect(self.ball.rect):
+                print("goal")
+                self.score += 1
+            else:
+                print("no goal")
+
+    def main_menu(self):
+        self.main_menu_active = True
+        bg = GameBackground()
+        button = Button(WIDTH/2 - 323/2, HEIGHT/2 - 50, 323, 100, "S T A R T")
+        while self.main_menu_active:
+            self.clock.tick(MENU_FPS)
+
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.running = False
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    mx, my = pg.mouse.get_pos()
+                    if button.check_click(mx, my):
+                        self.main_menu_active = False
+                        self.run()
+                        return
+
+            self.draw_window(bg, button)
+
+    def pause_menu(self):
+        button = Button(WIDTH/2 - 323/2, HEIGHT/2 - 50, 323, 100, "R E S U M E")
+        while True:
+            self.clock.tick(MENU_FPS)
+
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.running = False
+                    return
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    mx, my = pg.mouse.get_pos()
+                    if button.check_click(mx, my):
+                        return
+
+            pressed_keys = pg.key.get_pressed()
+            if pressed_keys[pg.K_ESCAPE] or pressed_keys[pg.K_TAB]:
+                return
+
+            self.draw_window(self.background, self.goal, self.gk, self.ball, button)
+
+    def run(self):
+        click_start = 0
+        m_hold = False
+        loading_bar = LoadingBar(0, 0, 200, 50, WHITE, RED, optimal_value=0.5)
+        side_view = SideView(self.ball)
+        self.running = True
+        while self.running:
+            # Régule l'éxecution de la boucle à 60 fois par seconde
+            self.clock.tick(GAME_FPS)
+            m = pg.Vector2(pg.mouse.get_pos())
+            t = pg.time.get_ticks() * MS_TO_S
+
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.running = False
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    click_start = t
+                    m_hold = True
+                if event.type == pg.MOUSEBUTTONUP and m_hold:
+                    m_hold = False
+                    loading_bar.val = 0
+                    self.ball.shoot(m, t, min(t - click_start, MAX_SHOOT_HOLD_TIME) * BALL_TIME_S_TO_STRENTGH)
+
+            pressed_keys = pg.key.get_pressed()
+            if pressed_keys[pg.K_a]:
+                self.ball.reset()
+                self.gk.reset()
+            if pressed_keys[pg.K_ESCAPE] or pressed_keys[pg.K_TAB]:
+                self.pause_menu()
+
+            self.gk.handle_movement(pressed_keys, t)
+            self.ball.handle_physics(t)
+            if m_hold:
+                loading_bar.val = min(t - click_start, MAX_SHOOT_HOLD_TIME)
+
+            self.draw_window(self.background, self.goal, self.gk, self.ball, loading_bar, side_view)
+
+
+def main():
+    pg.init()
+
+    game = Game()
+    game.main_menu()
+
+    pg.quit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ old/drawables_old.py
@@ -0,0 +1,243 @@
+import math
+
+import pygame as pg
+from constants import *
+from math import cos, sin, radians, degrees
+
+
+# GUI
+class MainMenuBackground:
+    def __init__(self):
+        pass
+
+
+class GameBackground:
+    def __init__(self):
+        self.rect = pg.Rect(0, 0, WIDTH, 304)
+
+    def draw(self, win):
+        win.fill(GREEN)
+        pg.draw.rect(win, BLUE, self.rect)
+
+
+class Button:
+    def __init__(self, x, y, width, heigth, text="BUTTON", font_size=48):
+        self.rect = pg.Rect(x, y, width, heigth)
+        font = pg.font.Font('freesansbold.ttf', font_size)
+        self.text = font.render(text, True, BLACK)
+        self.tw = self.text.get_width() / 2
+        self.th = self.text.get_height() / 2
+
+    def draw(self, win):
+        pg.draw.rect(win, WHITE, self.rect)
+        win.blit(self.text, (self.rect.centerx - self.tw, self.rect.centery - self.th))
+
+    def check_click(self, mx, my):
+        return self.rect.collidepoint(mx, my)
+
+
+class LoadingBar:
+    def __init__(self, left, top, width, heigth, bg_color, fill_color, optimal_color=(0, 0, 0), start_value=0,
+                 optimal_value=0, render_if_null=False):
+        self.rect = pg.Rect(left, top, width, heigth)
+        self.fill_rect = pg.Rect(left, top, width * start_value, heigth)
+        self.draw_opt_rect = optimal_value > 0
+        self.optimal_rect = pg.Rect(left + width * optimal_value, top, width * OPTIMAL_WIDTH_FACTOR, heigth)
+        self.width = width
+        self.bg_color = bg_color
+        self.fill_color = fill_color
+        self.optimal_color = optimal_color
+        self.val = start_value
+        self.render_if_null = render_if_null
+
+    def draw(self, win):
+        if not self.render_if_null and self.val == 0:
+            return
+
+        pg.draw.rect(win, self.bg_color, self.rect)
+        self.fill_rect.width = self.width * self.val
+        pg.draw.rect(win, self.fill_color, self.fill_rect)
+        if self.draw_opt_rect:
+            pg.draw.rect(win, self.optimal_color, self.optimal_rect)
+
+
+class Text:
+    def __init__(self, x, y, text, size, color, font='freesansbold.ttf', bold=False):
+        self.txt = pg.font.Font(font, size).render(text, True, color)
+        self.font, self.size, self.color = font, size, color
+        self.x, self.y = x, y
+
+    def set_txt(self, new_txt):
+        self.txt = pg.font.Font(self.font, self.size).render(new_txt, True, self.color)
+
+    def draw(self, win):
+        win.blit(self.txt, self.x, self.y)
+        #TODO: fix
+
+
+# Game elements
+class Goal:
+    def __init__(self):
+        self.rect = pg.Rect(GOAL_X, GOAL_Y, GOAL_WIDTH, GOAL_HEIGHT)
+
+    def draw(self, win):
+        pg.draw.rect(win, BLACK, self.rect)
+
+
+# TODO: make strong shot less precise
+
+class Ball:
+    def __init__(self, master):
+        self.master = master
+        self.rect = pg.Rect(BALL_START_X, BALL_START_Y, BALL_START_WIDTH, BALL_START_HEIGHT)
+        self.sprite = BALL_SPRITE
+        self.z = 0
+        self.shooting = False
+        self.t0 = 0
+        self.dir = pg.Vector3(0, 0, GOAL_Z)
+        self.vx = 0
+        self.vy = 0
+        self.vz = 0
+        self.catched = False
+        self.shoot_angle = 0
+
+    def size_by_depth(self):
+        return BALL_START_WIDTH + self.z * BALL_DEPTH_TO_SIZE
+
+    def height_adjustment_by_depth(self):
+        return self.z * BALL_DEPTH_TO_HEIGHT
+
+    @staticmethod
+    def target_y_adjustment(ty):
+        return 170 + (ty - 140) * 15 / 140
+
+    def draw(self, win):
+        size = self.size_by_depth()
+        win.blit(pg.transform.scale(self.sprite, (size, size)), (self.rect.x, self.rect.y))
+        # pg.draw.circle(win, WHITE, (self.rect.x, self.rect.y), self.radius_by_depth())
+        # pg.draw.line(win, BLUE, (self.rect.x, self.rect.y), (BALL_START_X + self.dir.x, BALL_START_Y + self.dir.y))
+        # pg.draw.line(win, RED, (BALL_START_X, BALL_START_Y), (BALL_START_X + self.dir.x, BALL_START_Y + self.dir.y))
+
+    def shoot(self, target, time_on_shoot, strength=1):
+        if self.shooting:
+            return
+
+        self.shooting = True
+        self.t0 = time_on_shoot
+        print(GOAL_Y + GOAL_HEIGHT)
+        print(target.y)
+        if target.y <= GOAL_Y + GOAL_HEIGHT:
+            target.y -= self.target_y_adjustment(target.y)
+        else:
+            target.y = BALL_TARGET_IF_FLOOR_SHOT
+
+        self.dir.x = target.x - BALL_START_X
+        self.dir.y = target.y - BALL_START_Y
+
+        self.shoot_angle = radians(self.dir.angle_to(pg.Vector3(self.dir.x, FLOOR_LINE.y, self.dir.z)))
+        spd = BALL_SPEED * strength
+        self.vx = spd * cos(self.shoot_angle) * self.dir.x
+        self.vy = spd * sin(self.shoot_angle) * self.dir.y
+        self.vz = spd * cos(self.shoot_angle) * self.dir.z
+
+    def handle_physics(self, t):
+        if self.catched:
+            self.rect.x = self.master.gk.rect.x + GOALKEEPER_WIDTH / 2 - BALL_END_WIDTH / 2
+            self.rect.y = self.master.gk.rect.y + GOALKEEPER_HEIGHT / 2 - BALL_END_HEIGHT / 2
+
+        if not self.shooting:
+            return
+
+        self.rect.x = self.vx * (t - self.t0) + BALL_START_X
+        self.z = self.vz * (t - self.t0)
+        if self.shoot_angle > 0.1:
+            self.rect.y = 0.5 * GRAVITY * (t - self.t0) ** 2 + self.vy * (
+                    t - self.t0) + self.height_adjustment_by_depth() + \
+                          BALL_START_Y
+        else:
+            self.rect.y = self.vy * (
+                    t - self.t0) + self.height_adjustment_by_depth() + \
+                          BALL_START_Y
+        # print("ball physics: ", self.rect.x, self.rect.y, self.z)
+
+        if self.z >= GOAL_Z:
+            self.shooting = False
+            self.master.check_goal()
+
+    def get_catched(self):
+        self.catched = True
+
+    def reset(self):
+        self.shooting = False
+        self.rect.x = BALL_START_X
+        self.rect.y = BALL_START_Y
+        self.z = BALL_START_Z
+        self.catched = False
+
+
+class GoalKeeper:
+    def __init__(self, master):
+        self.master = master
+        self.sprite = GOALKEEPER_SPRITE
+        self.rect = pg.Rect(GOALKEEPER_START_X, GOALKEEPER_START_Y, GOALKEEPER_WIDTH, GOALKEEPER_HEIGHT)
+        self.r_rect = self.rect
+        self.jumping = False
+        self.jumping_dir = 0
+        self.angle = 0
+        self.t0 = 0
+        self.x0 = 0
+        self.vy = 0
+        self.vx = 0
+
+    def draw(self, win):
+        if self.angle != 0:
+            rotated_sprite = pg.transform.rotate(self.sprite, -self.jumping_dir * self.angle)
+            rotated_rect = rotated_sprite.get_rect(center=self.sprite.get_rect(topleft=self.rect.topleft).center)
+            self.r_rect = rotated_rect
+            win.blit(rotated_sprite, (rotated_rect.x, rotated_rect.y))
+            # pg.draw.rect(win, PINK, self.r_rect)
+        else:
+            win.blit(self.sprite, (self.rect.x, self.rect.y))
+
+    def handle_movement(self, p_keys, t):
+        if self.jumping:
+            t -= self.t0
+            self.rect.x = self.vx * t + self.x0
+            self.rect.y = 0.5 * GRAVITY * t ** 2 - self.vy * t + GOALKEEPER_START_Y
+            self.angle += GOALKEEPER_TURN_SPEED
+
+            if self.angle != 0 and self.r_rect.y >= GOALKEEPER_START_Y + GOALKEEPER_HEIGHT - GOALKEEPER_WIDTH:  # 207
+                self.reset(False)
+            elif self.angle == 0 and self.rect.y >= GOAL_Y:
+                self.reset(False)
+
+            return
+
+        if p_keys[pg.K_q]:
+            self.rect.x -= GOALKEEPER_SPEED
+            self.jumping_dir = -1
+        if p_keys[pg.K_d]:
+            self.rect.x += GOALKEEPER_SPEED
+            self.jumping_dir = 1
+        if not p_keys[pg.K_q] and not p_keys[pg.K_d]:
+            self.jumping_dir = 0
+        if p_keys[pg.K_SPACE]:
+            self.jumping = True
+            self.t0 = t
+            self.x0 = self.rect.x
+            self.vx = self.jumping_dir * GOALKEEPER_JUMP_SPEED_X * cos(GOALKEEPER_JUMP_ANGLE)
+            self.vy = GOALKEEPER_JUMP_SPEED_Y * sin(GOALKEEPER_JUMP_ANGLE if self.jumping_dir != 0 else PI_HALVES)
+
+    def try_catching(self, ball):
+        if self.r_rect.colliderect(ball.rect):
+            ball.get_catched()
+            return True
+
+    def reset(self, reset_x=True):
+        if reset_x:
+            self.rect.x = GOALKEEPER_START_X
+        self.rect.y = GOALKEEPER_START_Y
+        self.r_rect = self.rect
+        self.angle = 0
+        self.jumping = False
+        self.jumping_dir = 0
